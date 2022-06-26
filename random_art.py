d7d12e8e5c651ad35b1ec9f0d5c5ddb0aac9bdfb
Keith Basil <98460561+keithbasil@users.noreply.github.com>
2022-06-26 17:28:57 +0530

    Update random_art.py
    It draws a random art.

--- random_art.py
+++ random_art.py
@@ -10,6 +10,7 @@ tim = Turtle()
 tim.shape("square")
 tim.color("firebrick3")
 
+# Colors for the random art
 
 colors = ["blue", "lawn green", "medium violet red", "crimson", "chocolate", "gold", "yellow", "dim gray",
 "steel blue", "dark cyan", "rosy brown", "LightSeaGreen", "IndianRed", "olive drab", "LightSalmon3", "LightCoral",
@@ -23,13 +24,15 @@ directions = [0, 90, 180, 270]
 tim.pensize(15)
 tim.speed("fastest")
 
-def random_walk():
-	for _ in range(500)
+# Function for the random art
+
+def random_art():
+	for _ in range(500):
 		tim.color(random.choice(colors))
 		tim.forward(30)
 		tim.setheading(random.choice(directions))
 
-random_walk()	
+random_art()	
 
 
 
