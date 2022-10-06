e26f6257762c631fbaf3747456d3849c83fe7de3
Keith Basil <98460561+keithbasil@users.noreply.github.com>
2022-10-06 16:17:17 +0530

    Updating version 2

--- credits_app/credits_app.py
+++ credits_app/credits_app.py
@@ -2,18 +2,25 @@ import time
 import random
 
 
-
+# Creating the welcome screen
 print("\nWelcome to the credits app! You can store your hours of credits,\n remove some of the credits,\nand you can add some of your credits!")
 
+# Checking what the user will choose 
 while True:
 	choice = input("Type 'a' to add credits or type 's' to show your credits and type 'r' to remove some credits or 'q' to quit: ").lower()
-	
+
+	# If user chosen a(add)	
 	if choice == "a":
-		w = input("Which credit you want to change. 1-'games' or 2-'movies': ") 
+		w = input("Which credit you want to change. 1-'games' or 2-'movies': ")
+
+		# if user chosen 1(games)
+
 		if w == "1":
 			number1 = int(input("Type the number of hours you want to add: "))
 			print("Loading...")
 			time.sleep(1)
+
+			# Adding credit to the file "game_credits.txt"
 
 			w = open("game_credits.txt", "r")
 			num = str(int(w.read()) + int(number1))
@@ -22,17 +29,22 @@ while True:
 			k.close()
 
 
+		# If user chosen 2 (movie)
 
 		elif w == "2":	
 			number1 = int(input("Type the number of hours you want to add: "))
 			print("Loading...")
 			time.sleep(1)
 
+			# Adding credit to file "movie_credits.txt"
+
 			w = open("movie_credits.txt", "r")
 			num = str(int(w.read()) + int(number1))
 			k = open("movie_credits.txt", "w")
 			k.write(num)
 			k.close()
+
+
 		else:
 			print("Sorry, I did not understand what you were trying to chose")	
 		
@@ -43,9 +55,14 @@ while True:
 		print("Added the number of hours to credits....Done!")
 
 
+	# If user chosen r (remove)
+
 	elif choice == "r":
 		
 		g = input("Which credit do you want to remove ? 1- games, 2-movies: ")
+
+		# if user chosen 1(games)
+
 		if g == "1":
 
 
@@ -53,6 +70,8 @@ while True:
 			print("Removing...")
 
 			w = open("game_credits.txt", "r")
+			
+			# Removing credits from file "game_credits.txt"
 
 			num = str(int(w.read()) - int(number2))
 			k = open("game_credits.txt", "w")
@@ -62,12 +81,16 @@ while True:
 			time.sleep(1)
 			print("Done!")
 
+		# if user chosen 2(movie)
+
 		elif g == "2":
 			number2 = int(input("Type the number of hours you want to remove: "))
 			print("Removing...")
 
 			
 			w = open("movie_credits.txt", "r")
+
+			# Removing credits from file "movie_credits.txt"
 
 			num = str(int(w.read()) - int(number2))
 			k = open("movie_credits.txt", "w")
@@ -80,6 +103,8 @@ while True:
 		else:
 			print("Sorry I didn't understand what to want to chose.")		
 
+	# if user chosen s (show)
+
 	elif choice == "s":
 		print("Your game credits is..")
 
@@ -91,7 +116,7 @@ while True:
 		w = open("movie_credits.txt", "r")
 		print(w.read())
 
-				
+	# If user chosen q (quit)				
 
 	elif choice == "q":
 		bye = ["Thank you for using the app! Bye!", "See you next time! :)", "Bye! Bye!", """
@@ -110,6 +135,3 @@ while True:
 		break
 	else:
 		print("Sorry, I didn't understand what you were trying to say. Please try again. ")
-
-
-	
